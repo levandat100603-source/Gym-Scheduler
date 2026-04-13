0209e11ec3ab6cccf2115ab00d0c6839c5d77ec0
webquanlyphongtro <levandat100603@gmail.com>
2026-04-13 16:47:53 +0700

    d

--- gym-scheduler-backend/routes/web.php
+++ gym-scheduler-backend/routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'gym-scheduler-backend',
+    ]);
 });
 
