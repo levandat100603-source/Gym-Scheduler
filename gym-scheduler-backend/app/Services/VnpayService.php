53f08afa4c6d3726ef0e27684d9d7514d77846f8
webquanlyphongtro <levandat100603@gmail.com>
2026-05-25 00:32:34 +0700

    abcdefg

--- gym-scheduler-backend/app/Services/VnpayService.php
+++ gym-scheduler-backend/app/Services/VnpayService.php
@@ -44,7 +44,7 @@ class VnpayService
             'secureHash' => $secureHash,
         ]);
 
-        return $gatewayUrl . '?' . $query . '&vnp_SecureHash=' . $secureHash;
+        return $gatewayUrl . '?' . $query . '&vnp_SecureHashType=SHA512&vnp_SecureHash=' . $secureHash;
     }
 
     public function verifyCallback(array $input): bool
